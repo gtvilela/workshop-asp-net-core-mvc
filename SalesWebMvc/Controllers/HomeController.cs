dad6572fcc0a1510ff0f9375e4cdc55cac466105
Gabriela Vilela <gabrielathalitasv@gmail.com>
2019-10-22 11:03:26 -0300

    First Migration and CRUD Departments

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
@@ -14,7 +14,7 @@ namespace SalesWebMvc.Controllers
         {
             return View();
         }
-
+        
         public IActionResult Privacy()
         {
             return View();
